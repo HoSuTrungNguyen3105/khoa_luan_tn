8b2e22e5968895d7ae57ef10df586e86b3020f7b
Nguyen <trungnguyenhs3105@gmail.com>
2024-11-15 22:11:04 +0700

    add

--- Server/index.js
+++ Server/index.js
@@ -6,8 +6,8 @@ import cors from 'cors'
 import AuthRoute from './Routes/AuthRoute.js'
 import UserRoute from './Routes/UserRoute.js'
 import PostRoute from './Routes/PostRoute.js'
-import ChatRoute from './Routes/ChatRoute.js'
-import MessageRoute from './Routes/MessageRoute.js'
+//import ChatRoute from './Routes/ChatRoute.js'
+//import MessageRoute from './Routes/MessageRoute.js'
 
 const app = express();
 app.use(cors()); // Kích hoạt CORS cho toàn bộ ứng dụng
@@ -17,7 +17,7 @@ app.use(bodyparser.urlencoded({limit: "20mb", extended: true}));
 
 dotenv.config();
 
-mongoose.connect(process.env.MONGODB)
+mongoose.connect('mongodb+srv://lostnfound:k2HOMRjQjlQ4zr5t@cluster0.sxv75.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 .then(() => {
     app.listen(5001,()=>{
         console.log(`Server is running on port ${process.env.PORT}`);
@@ -27,5 +27,5 @@ mongoose.connect(process.env.MONGODB)
 app.use('/auth', AuthRoute)
 app.use('/user', UserRoute)
 app.use('/post', PostRoute)
-app.use('/chat', ChatRoute)
-app.use('/message', MessageRoute)
+//app.use('/chat', ChatRoute)
+//app.use('/message', MessageRoute)
