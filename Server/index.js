b356a64651efc510d75dac24ab93622cde4414e2
Nguyen <trungnguyenhs3105@gmail.com>
2025-02-06 17:54:41 +0700

    commit x7

--- Server/index.js
+++ Server/index.js
@@ -1,11 +1,8 @@
 import express from "express";
 import bodyparser from "body-parser";
-import mongoose from "mongoose";
 import dotenv from "dotenv";
 import cors from "cors";
 import cookieParser from "cookie-parser";
-import session from "express-session";
-import MongoStore from "connect-mongo";
 
 // Import Routes
 import AdminRoute from "./Routes/AdminRoute.js";
@@ -15,46 +12,21 @@ import PostRoute from "./Routes/PostRoute.js";
 import AdvRoute from "./Routes/AdvRoute.js";
 import MessageRoute from "./Routes/MessageRoute.js";
 import { app, server } from "./lib/socket.js";
+import mongoose from "mongoose";
+const PORT = process.env.PORT || 5000;
 
-const PORT = process.env.PORT || 5001;
-const MONGODB_URI =
-  "mongodb+srv://lostnfound:k2HOMRjQjlQ4zr5t@cluster0.sxv75.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
-// Cors configuration
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL,
+    origin: "http://localhost:3000",
     credentials: true,
-    methods: "GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS",
   })
 );
-
-// Session configuration with MongoStore
-app.use(
-  session({
-    secret: "56748390657848848448484747336363363636363363636",
-    resave: false,
-    saveUninitialized: false, // Thay đổi thành false để tối ưu hiệu suất
-    // store: MongoStore.create({
-    //   mongoUrl: process.env.MONGODB_URI,
-    //   ttl: 24 * 60 * 60, // Thời gian session tồn tại (1 ngày)
-    //   autoRemove: "native", // Tự động xóa session hết hạn
-    // }),
-    cookie: {
-      secure: process.env.NODE_ENV === "production", // Tự động bật secure trong production
-      maxAge: 24 * 60 * 60 * 1000, // 1 ngày
-      sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
-    },
-  })
-);
-
 app.use(cookieParser());
 app.use(bodyparser.json({ limit: "20mb", extended: true }));
 app.use(bodyparser.urlencoded({ limit: "20mb", extended: true }));
 
 dotenv.config();
 
-// Database connection
 mongoose
   .connect(
     "mongodb+srv://lostnfound:k2HOMRjQjlQ4zr5t@cluster0.sxv75.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
@@ -65,7 +37,6 @@ mongoose
     });
   });
 
-// Routes
 app.use("/api/admin", AdminRoute);
 app.use("/api/auth", AuthRoute);
 app.use("/api/user", UserRoute);
